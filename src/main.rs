c88df15c5a4ee50a139dc178a242a55172a448cb
Salvador Guzman <guzmansalv@gmail.com>
2018-06-21 01:30:58 -0700

    HOLY SHIT FINALLY DID IT THAT TOOK HOURS

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,38 @@
-fn main() {
-    println!("start");
+extern crate actix_web;
+
+use actix_web::{App, Path, server};
+
+fn init_json() -> Vec<String> {
     let paths = std::fs::read_dir("./data").unwrap();
+    let mut collection: Vec<String> = Vec::new();
+    for path in paths {
+        let full_path = path.unwrap().path();
 
-    let coll = {
-        let mut collection: Vec<String> = Vec::new();
-        for path in paths {
-            let full_path = path.unwrap().path();
+        let contents = std::fs::read_to_string(full_path).unwrap();
+        collection.push(contents);
+    }
+    collection
+}
 
-            let contents = std::fs::read_to_string(full_path).unwrap();
-            collection.push(contents);
-        }
-        collection
-    };
+fn init() {
+    println!("start");
 
-    println!("done {}", coll.len());
+    server::new(|| {
+        App::new()
+            .resource("/{name}", |r| {
+                let items = init_json();
+                r.get().with(move|path: Path<usize>| {
+                    let value: usize = *path;
+                    let value = &items[value];
+                    value.clone()
+                }
+            )})
+    })
+        .bind("127.0.0.1:8000")
+        .expect("Can not bind to port 8000")
+        .run();
+}
+
+fn main() {
+    init();
 }
